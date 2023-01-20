b4408ea72867bd0f6ed68d097befe7a760c52c30
thfirmin <thiagofirmino2001@gmail.com>
2023-01-20 05:30:11 -0300

    Getting time

--- philo/philosophers.c
+++ philo/philosophers.c
@@ -6,7 +6,7 @@
 /*   By: thfirmin <thfirmin@student.42.rio>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/20 04:54:19 by thfirmin          #+#    #+#             */
-/*   Updated: 2023/01/20 04:55:02 by thfirmin         ###   ########.fr       */
+/*   Updated: 2023/01/20 05:25:39 by thfirmin         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,6 +14,14 @@
 
 int	main(void)
 {
-	printf ("Called!\n");
+	int	ret;
+	struct timeval tv;
+	struct timezone tz;
+
+	ret = gettimeofday(&tv, &tz);
+	printf ("Return: %d\n", ret);
+	printf ("timeval:\ntv_sec = %li\ntv_usec = %i\n", (tv.tv_sec * 1000), tv.tv_usec);
+	printf ("timezone:\ntz_minuteswest = %d\ntz_dsttime = %d\n", tz.tz_minuteswest, tz.tz_dsttime);
+	printf ("MILISECONDS: %li\n", ((tv.tv_sec * 1000) + (tv.tv_usec / 1000)));
 	return (0);
 }
